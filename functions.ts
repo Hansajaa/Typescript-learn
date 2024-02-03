ef1f5e83965e492473af77f9f9edfc51b3b6a8e9
Hansajaa <nilanahansaja21@gmail.com>
2024-02-03 15:23:05 +0530

    function part one

---
+++ functions.ts
@@ -0,0 +1,26 @@
+
+// functions
+function greet(name:string){
+
+    console.log("Hello "+name.toUpperCase());
+
+}
+
+greet("hansaja");
+
+// Arrow functions
+let login = (name: string, email: string, isPaid :boolean = false) =>{
+
+}
+
+login("Hansaja","Hansaja@gmail.com")
+
+
+function addTwo(num: number){
+    return num+2
+    // return "hello"
+}
+
+let final:number = addTwo(5)
+
+
