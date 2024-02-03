ace9ab6a05ba285179f76f8b604c9c176f60bb55
Hansajaa <nilanahansaja21@gmail.com>
2024-02-03 16:22:28 +0530

    function part two

--- functions.ts
+++ functions.ts
@@ -16,11 +16,40 @@ let login = (name: string, email: string, isPaid :boolean = false) =>{
 login("Hansaja","Hansaja@gmail.com")
 
 
-function addTwo(num: number){
+// We can define a return type of function
+function addTwo(num: number):number{
     return num+2
-    // return "hello"
+    //  return "hello"
 }
 
-let final:number = addTwo(5)
+let final = addTwo(5)
+
+
+// Also we can define return typr to the arrow function
+
+let getHello = (): string => {
+    return "Hello"
+}
+
+var superHeroes = ["Batman","Spiderman","Ironman"]
+// var superHeroes=[1,2,3]
+
+// Typescript implesitly detect the type of array and we don't have to define a type 
+superHeroes.map((hero:string): string=>{
+    return `This is super hero ${hero}`
+})
+
+
+// We don't return a value we use to void
+function errorHandle():void{
+    console.log("Exception")
+}
+
+// never
+function throwError(msg: string):never{
+    throw new Error(msg);
+}
+
+
 
 
