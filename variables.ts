64a0c6b6ceba6624a969f4bcb91b5d16595e00ba
Hansajaa <nilanahansaja21@gmail.com>
2024-02-03 14:40:00 +0530

    first commit

---
+++ variables.ts
@@ -0,0 +1,24 @@
+// variables
+
+// string
+
+let myName:string = "Nilana";
+
+//myName=42;  string cannot assign a number
+
+// number
+
+let myNum:number
+
+myNum=42;  
+myNum=1.2344444; //typescript hasn't special runtime value for integer. It's simply called number
+
+
+let myBool:boolean
+
+myBool=true;
+myBool=false;
+// myBool=23; cannot assign number to the boolean type variable
+
+
+
